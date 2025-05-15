c2bff2d9f5af78d65ff4d91239b27b7ec35eef97
Yves <yvesmorello@hotmail.com>
2025-05-14 22:49:15 -0300

    first commit

---
+++ src/main/java/com/example/atividadeTaskJWT/AtividadeTaskJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.atividadeTaskJWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeTaskJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeTaskJwtApplication.class, args);
+	}
+
+}

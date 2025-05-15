30f69e90c4dea2fc1e4d332ad5406a99cba9f837
Yves <yvesmorello@hotmail.com>
2025-05-14 23:17:36 -0300

    criacao das controllers das entidades user e task

---
+++ src/main/java/com/example/atividadeTaskJWT/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.atividadeTaskJWT.controller;
+
+import com.example.atividadeTaskJWT.domain.User;
+import com.example.atividadeTaskJWT.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        User newUser = service.create(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id,@RequestBody User user){
+        return ResponseEntity.ok(service.update(id, user));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        return ResponseEntity.noContent().build();
+    }
+}

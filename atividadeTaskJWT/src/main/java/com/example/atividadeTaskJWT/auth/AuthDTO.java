eb21b174cf10513f08e26c3b3fd057d77aeb4c7f
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2025-05-21 17:28:14 -0300

    Delete atividadeTaskJWT directory

--- atividadeTaskJWT/src/main/java/com/example/atividadeTaskJWT/auth/AuthDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.atividadeTaskJWT.auth;
-
-public class AuthDTO {
-    public record AuthRequest(String username, String password) {}
-    public record AuthResponse(String token) {}
-}
--- atividadeTaskJWT/src/main/java/com/example/atividadeTaskJWT/repository/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.atividadeTaskJWT.repository;
-
-import com.example.atividadeTaskJWT.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
